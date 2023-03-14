7d0fcdf53ef49ae1d88a1ed410ab5c0f82fb84ca
George Berar <gberar@worldremit.com>
2023-03-14 15:09:07 +0200

    added missing files

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { FundingPaymentResolver } from './graphql/funding-payment.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory: async () => ({
+        autoSchemaFile: true,
+        playground: true,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [FundingPaymentResolver],
+})
+export class AppModule {}
---
+++ src/graphql/funding-payment.resolver.ts
@@ -0,0 +1,55 @@
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { FundingPayment } from './funding-payment.type';
+import { uuid } from 'uuidv4';
+
+@Resolver(() => FundingPayment)
+export class FundingPaymentResolver {
+  constructor() {}
+
+  @Query(() => [FundingPayment], {
+    name: 'fundingpayments',
+  })
+  async getFundingPayments(
+    @Args('count', {
+      type: () => Number,
+      description: 'The number of items to generate and return.',
+      nullable: false,
+    })
+    count: Number,
+  ): Promise<FundingPayment[]> {
+    const payments: FundingPayment[] = [];
+
+    for (let i = 0; i < count; i++) {
+      payments.push({
+        id: uuid(),
+        correspondent_id: uuid(),
+        tracker_payment_id: uuid(),
+        status: this.getRandomStatus(),
+        trade_currency_code: this.getRandomCurrencyCode(),
+        trade_date: new Date(),
+        planned_settlement_amount: this.getRandomNumber(1000),
+        planned_settlement_amount_usd: this.getRandomNumber(1000),
+        expected_settlement_date: new Date(),
+        actual_settlement_amount: this.getRandomNumber(1000),
+        actual_settlement_amount_usd: this.getRandomNumber(1000),
+        actual_settlement_date: new Date(),
+      });
+    }
+
+    return Promise.resolve(payments);
+  }
+
+  getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  getRandomStatus(): string {
+    const r = this.getRandomNumber(2);
+    return r == 0 ? 'IN_TRANSIT' : 'SETTLED';
+  }
+
+  getRandomCurrencyCode(): string {
+    const r = this.getRandomNumber(2);
+    return r == 0 ? 'EUR' : 'USD';
+  }
+}
