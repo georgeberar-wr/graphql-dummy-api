6ea7a15d3ac958b47877134fee1951799edd672d
George Berar <gberar@worldremit.com>
2023-03-15 15:54:44 +0200

    wip CORS

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    allowedHeaders: "Content-Type, Accept",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders:
+      "Content-Type, Accept, Access-Control-Allow-Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     credentials: true,
   });
   await app.listen(3000);
