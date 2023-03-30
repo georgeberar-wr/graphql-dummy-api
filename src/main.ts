2f85a4b782570162058dcc0dff045591c4f24e05
George Berar <gberar@worldremit.com>
2023-03-30 15:23:51 +0300

    fix redeploy

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    allowedHeaders:
-      "Content-Type, Accept, Access-Control-Allow-Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: "*",
+  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  //   allowedHeaders:
+  //     "Content-Type, Accept, Access-Control-Allow-Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  //   credentials: true,
+  // });
   await app.listen(3000);
 }
 bootstrap();
