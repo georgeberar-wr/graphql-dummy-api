c7d4b9677324a80cef6b908b5156cb3b7ed1e3af
George Berar <gberar@worldremit.com>
2023-03-30 15:53:24 +0300

    reverted

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,20 @@ import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { HelloController } from "./rest/hello.controller";
+import { CorrespondentBalanceResolver } from "./graphql/balances/correspondent-balances.resolver";
 
 @Module({
   imports: [
-    // GraphQLModule.forRootAsync<ApolloDriverConfig>({
-    //   driver: ApolloDriver,
-    //   useFactory: async () => ({
-    //     autoSchemaFile: true,
-    //     playground: true,
-    //     introspection: true,
-    //   }),
-    // }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory: async () => ({
+        autoSchemaFile: true,
+        playground: true,
+        introspection: true,
+      }),
+    }),
   ],
   controllers: [HelloController],
-  providers: [],
+  providers: [CorrespondentBalanceResolver],
 })
 export class AppModule {}
