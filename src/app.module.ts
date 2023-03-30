2891201ab39b80cb2ca599296669e77c99e37cd5
George Berar <gberar@worldremit.com>
2023-03-30 15:28:21 +0300

    remove graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,20 @@
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
-import { CorrespondentBalanceResolver } from "./graphql/balances/correspondent-balances.resolver";
 import { HelloController } from "./rest/hello.controller";
 
 @Module({
   imports: [
-    GraphQLModule.forRootAsync<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      useFactory: async () => ({
-        autoSchemaFile: true,
-        playground: true,
-        introspection: true,
-      }),
-    }),
+    // GraphQLModule.forRootAsync<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   useFactory: async () => ({
+    //     autoSchemaFile: true,
+    //     playground: true,
+    //     introspection: true,
+    //   }),
+    // }),
   ],
   controllers: [HelloController],
-  providers: [CorrespondentBalanceResolver],
+  providers: [],
 })
 export class AppModule {}
--- src/graphql/balances/correspondent-balances.resolver.ts
+++ src/graphql/balances/correspondent-balances.resolver.ts
@@ -1,102 +1,102 @@
-import { Args, Query, Resolver } from "@nestjs/graphql";
-import { Pagination } from "../common/common.type";
-import { v4 as uuidv4 } from "uuid";
-import {
-  CorrespondentBalance,
-  PaginatedCorrespondentBalance,
-} from "./correspondent-balance.type";
+// import { Args, Query, Resolver } from "@nestjs/graphql";
+// import { Pagination } from "../common/common.type";
+// import { v4 as uuidv4 } from "uuid";
+// import {
+//   CorrespondentBalance,
+//   PaginatedCorrespondentBalance,
+// } from "./correspondent-balance.type";
 
-import { uniqueNamesGenerator, names } from "unique-names-generator";
+// import { uniqueNamesGenerator, names } from "unique-names-generator";
 
-const BALANCES: CorrespondentBalance[] = [];
-let TOTAL_PAGES;
+// const BALANCES: CorrespondentBalance[] = [];
+// let TOTAL_PAGES;
 
-function getRandomNumber(max) {
-  return Math.floor(Math.random() * max);
-}
+// function getRandomNumber(max) {
+//   return Math.floor(Math.random() * max);
+// }
 
-function getRandomCurrencyCode(): string {
-  const r = getRandomNumber(2);
-  return r == 0 ? "EUR" : "USD";
-}
+// function getRandomCurrencyCode(): string {
+//   const r = getRandomNumber(2);
+//   return r == 0 ? "EUR" : "USD";
+// }
 
-function load() {
-  for (let i = 0; i < 125; i++) {
-    const currency = getRandomCurrencyCode();
+// function load() {
+//   for (let i = 0; i < 125; i++) {
+//     const currency = getRandomCurrencyCode();
 
-    BALANCES.push({
-      correspondent: {
-        id: uuidv4(),
-        name: uniqueNamesGenerator({
-          style: "capital",
-          dictionaries: [names],
-        }),
-      },
-      pending: {
-        amount: getRandomNumber(1000),
-        currency: currency,
-      },
-      current: {
-        amount: getRandomNumber(1000),
-        currency: currency,
-      },
-    });
-  }
+//     BALANCES.push({
+//       correspondent: {
+//         id: uuidv4(),
+//         name: uniqueNamesGenerator({
+//           style: "capital",
+//           dictionaries: [names],
+//         }),
+//       },
+//       pending: {
+//         amount: getRandomNumber(1000),
+//         currency: currency,
+//       },
+//       current: {
+//         amount: getRandomNumber(1000),
+//         currency: currency,
+//       },
+//     });
+//   }
 
-  TOTAL_PAGES = Math.floor(BALANCES.length / 25);
-}
+//   TOTAL_PAGES = Math.floor(BALANCES.length / 25);
+// }
 
-load();
-
-@Resolver(() => PaginatedCorrespondentBalance)
-export class CorrespondentBalanceResolver {
-  constructor() {}
-
-  @Query(() => PaginatedCorrespondentBalance, {
-    name: "getCorrespondentBalances",
-  })
-  async getCorrespondentBalances(
-    @Args("pagination", {
-      type: () => Pagination,
-      nullable: true,
-      defaultValue: { page: 0, limit: 25 },
-    })
-    pagination: Pagination
-  ): Promise<PaginatedCorrespondentBalance> {
-    const balances: CorrespondentBalance[] = [];
-
-    for (let i = pagination.page + 25; i < pagination.page + 50; i++) {
-      balances.push(BALANCES[i]);
-    }
-
-    const response: PaginatedCorrespondentBalance = {
-      balances: balances,
-      metadata: {
-        total_items: BALANCES.length,
-        total_pages: TOTAL_PAGES,
-        next_page: pagination.page + 1 > TOTAL_PAGES ? 0 : pagination.page + 1,
-        previous_page: pagination.page - 1 < 0 ? 0 : pagination.page - 1,
-      },
-    };
-
-    return Promise.resolve(response);
-  }
-}
+// load();
 
 // @Resolver(() => PaginatedCorrespondentBalance)
-// export class CorrespondentBalanceV2Resolver {
+// export class CorrespondentBalanceResolver {
 //   constructor() {}
 
-//   @Query(() => [CorrespondentBalance], {
-//     name: "correspondentBalances",
+//   @Query(() => PaginatedCorrespondentBalance, {
+//     name: "getCorrespondentBalances",
 //   })
-//   async getCorrespondentBalances(): Promise<CorrespondentBalance[]> {
+//   async getCorrespondentBalances(
+//     @Args("pagination", {
+//       type: () => Pagination,
+//       nullable: true,
+//       defaultValue: { page: 0, limit: 25 },
+//     })
+//     pagination: Pagination
+//   ): Promise<PaginatedCorrespondentBalance> {
 //     const balances: CorrespondentBalance[] = [];
 
-//     for (let i = 0; i < BALANCES.length; i++) {
+//     for (let i = pagination.page + 25; i < pagination.page + 50; i++) {
 //       balances.push(BALANCES[i]);
 //     }
 
-//     return Promise.resolve(balances);
+//     const response: PaginatedCorrespondentBalance = {
+//       balances: balances,
+//       metadata: {
+//         total_items: BALANCES.length,
+//         total_pages: TOTAL_PAGES,
+//         next_page: pagination.page + 1 > TOTAL_PAGES ? 0 : pagination.page + 1,
+//         previous_page: pagination.page - 1 < 0 ? 0 : pagination.page - 1,
+//       },
+//     };
+
+//     return Promise.resolve(response);
 //   }
 // }
+
+// // @Resolver(() => PaginatedCorrespondentBalance)
+// // export class CorrespondentBalanceV2Resolver {
+// //   constructor() {}
+
+// //   @Query(() => [CorrespondentBalance], {
+// //     name: "correspondentBalances",
+// //   })
+// //   async getCorrespondentBalances(): Promise<CorrespondentBalance[]> {
+// //     const balances: CorrespondentBalance[] = [];
+
+// //     for (let i = 0; i < BALANCES.length; i++) {
+// //       balances.push(BALANCES[i]);
+// //     }
+
+// //     return Promise.resolve(balances);
+// //   }
+// // }
